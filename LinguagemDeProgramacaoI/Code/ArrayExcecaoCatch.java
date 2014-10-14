4e6331db2f65ca40318b0fdb143718c64c5d7f01
Andre Bem <andrebemfs@gmail.com>
2014-10-14 09:15:54 -0300

    [lp1-p06] Adding Try-catch-finally examples (Java) and updated the reference slides on Exceptions

---
+++ LinguagemDeProgramacaoI/Code/ArrayExcecaoCatch.java
@@ -0,0 +1,13 @@
+public class ArrayExcecao {
+  /*...*/
+
+  public static void main(String[] args) {
+    try {
+      naPosicao(-1);
+    }
+    catch (ArrayIndexOutOfBoundsException ex) {
+      ex.printStackTrace(); //mostra dump no console
+    }
+  }
+}
+ 
---
+++ LinguagemDeProgramacaoI/Code/TryCatchExemplo.java
@@ -0,0 +1,9 @@
+try {
+  //Trecho que possivelmente lanca uma excecao
+}
+catch (NomeDaExcecao excecao) {
+  //Possibilita capturar a excecao e definir
+}
+finally {
+  //Epilogo da excecao (consequencias)
+}
