e369684f8cbc699ebf8cc4dde5916ece7e514acb
André Bem <fbsilva.andre@gmail.com>
2014-10-13 22:25:07 -0300

    [lp1-p06] Adding Exception code samples

---
+++ LinguagemDeProgramacaoI/Code/ArrayExcecao.java
@@ -0,0 +1,13 @@
+public class ArrayExcecao {
+	Object[] objetos_;
+
+	public Object naPosicao(int indice) 
+			throws ArrayIndexOutOfBoundsException {
+		
+		if (indice < 0 || indice > objetos_.length) {
+			throw new ArrayIndexOutOfBoundsException(indice);
+		}
+		return objetos_[indice];
+	}
+}
+ 
---
+++ LinguagemDeProgramacaoI/Code/HerdaExcecao.java
@@ -0,0 +1,1 @@
+public class HerdaExcecao extends Exception {}
