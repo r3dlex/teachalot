35ad05a318e5f9668667c1645d0dcf3f4a8c1db2
André Bem <fbsilva.andre@gmail.com>
2014-10-22 19:30:44 -0200

    [lp1-p07] Adding an example of how to write to a text file. Adding two exercises

--- LinguagemDeProgramacaoI/Code/LendoTexto.java
+++ LinguagemDeProgramacaoI/Code/LendoTexto.java
@@ -1,21 +1,20 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class LendoTexto {
 	public static void main(String[] args) throws IOException {
-		BufferedReader reader = null;
+		BufferedReader leitor = null;
 		try {
-			reader = new BufferedReader(new FileReader(
+			leitor = new BufferedReader(new FileReader(
 					"C:\\Caminho\\Para\\O\\Arquivo.txt"));
 			String linha = null;
-			while ((linha = reader.readLine()) != null) {		
+			while ((linha = leitor.readLine()) != null) {		
         /* ... */
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			reader.close();
+			leitor.close();
 		}
 	}
 }
