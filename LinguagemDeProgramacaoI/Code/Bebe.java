b603439c4e1457e877beeb3ce0494cd8934cc809
André Bem <fbsilva.andre@gmail.com>
2014-11-05 08:03:19 -0200

    [lp1-e02] Fixing problems on first exam. Slightly changing second exam so it won't be exactly the same thing

--- LinguagemDeProgramacaoI/Code/Bebe.java
+++ LinguagemDeProgramacaoI/Code/Bebe.java
@@ -1,7 +1,7 @@
 public class Bebe {
-  String _nome;
-  double _peso;
-  double _altura;
+  protected String _nome;
+  protected double _peso;
+  protected double _altura;
 
   Bebe(String nome) {
     _nome = nome;
