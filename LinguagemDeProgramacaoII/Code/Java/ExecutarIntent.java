3a6893c1dcc5d85d82794bc52aaca85fb2cc3860
André Bem <fbsilva.andre@gmail.com>
2015-06-01 08:19:03 -0300

    [lp2-p06] Adding lectures about dynamic fragments

---
+++ LinguagemDeProgramacaoII/Code/Java/ExecutarIntent.java
@@ -0,0 +1,5 @@
+//Cria um Intent para inicializar OlaAndroid
+Intent intentOlaAndroid = new Intent(TelaDeLogin.this,
+    OlaAndroid.class);
+// Usa Intent para inicializar a Activity
+startActivity(intentOlaAndroid);
