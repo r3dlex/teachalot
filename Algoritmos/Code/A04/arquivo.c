bc78bd58322052f85772975a52b3af53dd35bfe2
André Bem <fbsilva.andre@gmail.com>
2015-10-26 07:40:08 -0200

    [algo] Codes

---
+++ Algoritmos/Code/A04/arquivo.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  //struct que identifica um arquivo para ler (entrada)
+  FILE *ponteiro_arquivo_de_entrada;
+  //struct que identifica um arquivo para escrita (saida)
+  FILE *ponteiro_arquivo_de_saida;
+  char modo[] = "r";
+  char arquivo_de_entrada[] = "entrada.txt";
+  char arquivo_de_saida[] = "saida.txt";
+
+  //Abre o arquivo em modo de leitura
+  ponteiro_arquivo_de_entrada = fopen(arquivo_de_entrada, modo);
+
+  if (ponteiro_arquivo_de_entrada == NULL) {
+    fprintf(stderr, "Impossivel abrir arquivo de entrada %s\n", arquivo_de_entrada);
+    exit(1);
+  }
+
+  //Abre o arquivo em modo de escrita e o
+  //cria automaticamente se nao existir
+  ponteiro_arquivo_de_saida = fopen(arquivo_de_saida, "w");
+
+  if (ponteiro_arquivo_de_saida == NULL) {
+    fprintf(stderr, "Impossivel abrir o arquivo de saida %s\n",
+        arquivo_de_saida);
+    exit(1);
+  }
+}
