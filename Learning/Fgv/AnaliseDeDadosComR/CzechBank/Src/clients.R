613073fa10371d4ad07871c8f3caee1c1d553e15
André Bem <fbsilva.andre@gmail.com>
2017-11-27 22:17:24 -0200

    [fgv-ar] Normalized clients table

--- Learning/Fgv/AnaliseDeDadosComR/CzechBank/Src/clients.R
+++ Learning/Fgv/AnaliseDeDadosComR/CzechBank/Src/clients.R
@@ -1,17 +1,45 @@
 library(dplyr)
 
-string.fixedsplit <- function(string, size){
+FixedSizeStringSplit <- function(string, size){
   pat <- paste0('(?<=.{',size,'})')
   strsplit(string, pat, perl=TRUE)
 }
 
-# birthnumber is in the format YYMMDD - if it is a woman it is DD + 50
-clients.genderfromdate  <- function(birthnumber)
+GetDateFromBirthNumber <- function(birthnumber)
 {
-  astring <- string.fixedsplit(sprintf("%d", birthnumber), 2)
-  yy <- as.integer(astring[[1]][1])
-  mm <- as.integer(astring[[1]][2])
-  dd <- as.integer(astring[[1]][3])
+  return (FixedSizeStringSplit(sprintf("%d", birthnumber), 2)[[1]])
+}
+
+# birthnumber is in the format YYMMDD - if it is a woman it is DD + 50
+GenderFromDate <- function(birthnumber)
+{
+  yymmdd <- GetDateFromBirthNumber(birthnumber)
+
+  yy <- as.integer(yymmdd[1])
+  mm <- as.integer(yymmdd[2])
+  dd <- as.integer(yymmdd[3])
 
   return (ifelse(mm < 50,  0 , 1))
 }
+
+GetBirthDateYyMmDd <- function(birthnumber)
+{
+  yymmdd <- GetDateFromBirthNumber(birthnumber)
+
+  yy <- as.integer(yymmdd[1])
+  mm <- as.integer(yymmdd[2])
+  mm <- ifelse(mm < 50, mm, mm - 50)
+  dd <- as.integer(yymmdd[3])
+
+  return (sprintf('19%s/%s/%s', yy, mm, dd))
+}
+
+ReadClientsDataFrame <- function()
+{
+  clients.df <- (read.csv2(GetDataFilepath("client.asc")))
+  clients.df$gender <- sapply(clients.df$birth_number, GenderFromDate)
+  clients.df$birth_date <- sapply(clients.df$birth_number, GetBirthDateYyMmDd)
+
+  return (clients.df)
+}
+
--- Learning/Fgv/AnaliseDeDadosComR/CzechBank/Src/script.R
+++ Learning/Fgv/AnaliseDeDadosComR/CzechBank/Src/script.R
@@ -1,1 +1,28 @@
+source.directory <- "Src"
+data.directory <- "Data"
 
+GetDataFilepath <- function(filename)
+{
+  return (sprintf("%s/%s", data.directory, filename))
+}
+
+GetSrcFilepath <- function(filename)
+{
+  return (sprintf("%s/%s", source.directory, filename))
+}
+
+RunSrcFromPath <- function(filename)
+{
+  source(GetSrcFilepath(filename))
+}
+
+# Sets working directory to this dir
+#setwd.scriptdir <- function()
+#{
+  #this.dir <- dirname(parent.frame(2)$ofile)
+  #setwd(sprintf("%s/..", this.dir))
+#}
+
+#setwd.scriptdir()
+
+RunSrcFromPath("clients.R")
