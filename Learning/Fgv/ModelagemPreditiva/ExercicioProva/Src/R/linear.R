73522da15af1cc450b415ee60f66afe0aadf8569
André Bem <andrebemfs@gmail.com>
2018-01-18 02:04:53 -0200

    [fgv-mp] Adds missing learn and test sets usage for linear regression

--- Learning/Fgv/ModelagemPreditiva/ExercicioProva/Src/R/linear.R
+++ Learning/Fgv/ModelagemPreditiva/ExercicioProva/Src/R/linear.R
@@ -35,20 +35,33 @@ ReadDataFromFile <- function()
   NormalizeTeba
 }
 
+GetSampleFlags <- function(data, percentage, size)
+{
+  sample(1:size, size * percentage, replace=F)
+}
+
 # Applies linear regression
 teba.df <- ReadDataFromFile()
+
+# Equivalent sample / createDataPartition calls
+sample.flags <- GetSampleFlags(data = teba.df, percentage = 0.6, size = nrow(teba.df))
+#sample.flags <- createDataPartition(teba.df$cancel, p = 0.6, list=F)
+teba.learn <- teba.df[sample.flags,]
+teba.test <- teba.df[-sample.flags,]
+
 regression.formula <- cancel ~ idade + linhas + temp_cli + renda + fatura + temp_rsd + tvcabo + debaut
-teba.regression <- glm(regression.formula, family = binomial(), data = teba.df)
+teba.regression <- glm(regression.formula, family = binomial(), data = teba.learn)
 summary(teba.regression)
 
 # Tries to guess a threshold value (0.6) and see what is the overall error in this case
-predict.cancel <- predict(teba.regression, type = "response")
-responses <- as.factor(ifelse(predict.cancel >= 0.6, "Y", "N"))
-CrossTable(teba.df$cancel, responses, prop.chisq=F, prop.t = F, digit = 2)
-teba.cv <- cv.glm(
-    data=teba.df, 
-    glmfit = teba.regression,
-    K = 10) #  K = number of groups to split the date
+predict.cancel <- predict(teba.regression, newdata = teba.test, type = "response")
+responses <- as.factor(ifelse(predict.cancel >= 0.6, 0, 1))
+CrossTable(teba.test$cancel, responses, prop.chisq=F, prop.t = F, digit = 2)
+
+#teba.cv <- cv.glm(
+    #data=teba.df, 
+    #glmfit = teba.regression,
+    #K = 10) #  K = number of groups to split the date
 
 # Leave one-out cross validation (too slow!)
 #teba.loocv <- cv.glm(
@@ -56,32 +69,33 @@ teba.cv <- cv.glm(
     #glmfit = teba.regression) 
 
 predict.calibration <- discretize(predict.cancel, method = "frequency", categories = 6)
-predict.table <- table(predict.calibration, teba.df$cancel)
+predict.table <- table(predict.calibration, teba.test$cancel)
 print(predict.table)
 
 PrintFixedIntervals <- function(data, predict.data)
 {
   #From the observations one could divide into 5 main categories
-  limits <- c(0, 0.2, 0.45, 0.55, 0.7, 1)
-  fixed.calibration <- discretize(predict.data, method = "fixed", categories = limits)
+  #limits <- c(0, 0.2, 0.45, 0.55, 0.7, 1)
+  fixed.calibration <- discretize(predict.data, method = "frequency", categories = 5)
   fixed.table <- table(fixed.calibration, data)
   print(fixed.table)
+  CrossTable(fixed.calibration, data, prop.c = F, prop.t = F, prop.chisq = F, digit = 3)
 }
 
-PrintFixedIntervals(teba.df$cancel, predict.cancel)
+#PrintFixedIntervals(teba.test$cancel, predict.cancel)
 
 # Will show the best linear regression variables
-step(teba.regression)
-final.formula <- cancel ~ idade + temp_cli + renda + fatura
-final.regression <- glm(final.formula, family = binomial(), data = teba.df)
+final.regression <- step(teba.regression)
+#final.formula <- cancel ~ idade + temp_cli + renda + fatura
+#final.regression <- glm(final.formula, family = binomial(), data = teba.df)
 
-predict.final <- predict(final.regression, type = "response")
-PrintFixedIntervals(teba.df$cancel, predict.final)
-final.responses <- as.factor(ifelse(predict.final >= 0.7, "Y", "N"))
-CrossTable(teba.df$cancel, final.responses, prop.chisq=F, prop.t = F, digit = 2)
+predict.final <- predict(final.regression, newdata = teba.test, type = "response")
+PrintFixedIntervals(teba.test$cancel, predict.final)
+final.responses <- as.factor(ifelse(predict.final >= 0.6, 0, 1))
+CrossTable(teba.test$cancel, final.responses, prop.chisq=F, prop.t = F, digit = 2)
 # KS = Kolmogorov-Smirnov
 # AUC = AUROC (Area under ROC)
-summary(HMeasure(teba.df$cancel, predict.final))
+print(HMeasure(teba.test$cancel, predict.final)$metrics)
 # Sensitivity = True positive Rate
 # Specificity = False positive Rate
-teba.roc <- roc(teba.df$cancel, predict.final)
+#teba.roc <- roc(teba.test$cancel, predict.final)
